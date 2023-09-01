65540dacf686ed5bb3384c52fe4e70ad33cfaacd
achang302 <alliechang8@gmail.com>
2023-09-01 14:43:41 -0700

    edited addition cycles (added minLength and maxLength options, finalized createCycle) and added warmup class

--- src/AdditionCycles.java
+++ src/AdditionCycles.java
@@ -3,31 +3,79 @@ import java.util.ArrayList;
 
 public class AdditionCycles {
     public static void main(String[] args) {
-        //placeholder
+        additionCycle();
+        System.out.println("max lengths:");
+        maxAdditionCycleLength();
+        System.out.println("min lengths:");
+        minAdditionCycleLength();
     }
 
     public static void additionCycle() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                System.out.println(i + ", " + j + ": " + createCycle(i, j) + ", length: " + cycleLength(createCycle(i, j)));
+                System.out.println(i + ", " + j + ": length " + createCycle(i, j));
             }
         }
     }
 
-    public static ArrayList<Integer> createCycle(int first, int second) {
-        int counter;
-        ArrayList<Integer> cycle = new ArrayList<>();
-        cycle.add(first);
-        cycle.add(second);
-        for (int i = 2; cycle.get(0) + cycle.get(1) != cycle.get(i - 1) + cycle.get(i - 2); i++) {
-            if (cycle.get(i - 1) + cycle.get(i - 2) > 9) {
-                cycle.add((cycle.get(i - 1) + cycle.get(i - 2)) % 10);
+    public static int createCycle(int first, int second) {
+        int counter = 2;
+        int prev = (first + second) % 10;
+        int current = (second + prev) % 10;
+        boolean end = false;
+        while (!end) {
+            int temp = current;
+            current = (current + prev) % 10;
+            prev = temp;
+            counter++;
+            if (prev == first) {
+                if (current == second) {
+                    end = true;
+                }
+            } else if (prev == second) {
+                if (current == first) {
+                    end = true;
+                }
             }
         }
-        return cycle;
+        return counter;
     }
 
-    public static int cycleLength(ArrayList<Integer> cycle) {
-        return -1;
+    public static void maxAdditionCycleLength() {
+        int maxLength = Integer.MIN_VALUE;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (createCycle(i, j) >= maxLength) {
+                    maxLength = createCycle(i, j);
+                }
+            }
+        }
+        System.out.println(maxLength);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (createCycle(i, j) == maxLength) {
+                    System.out.println(i + ", " + j);
+                }
+            }
+        }
+    }
+
+    public static void minAdditionCycleLength() {
+        int minLength = Integer.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (createCycle(i, j) <= minLength) {
+                    minLength = createCycle(i, j);
+                }
+            }
+        }
+        System.out.println(minLength);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (createCycle(i, j) == minLength) {
+                    System.out.println(i + ", " + j);
+                }
+            }
+        }
     }
 }
---
+++ src/WarmUp.java
@@ -0,0 +1,14 @@
+public class WarmUp {
+    public static void main(String[] args) {
+        String[] names = {"Bilbo", "Elmo", "Wally"};
+        String[] colors = {"Gray", "Rainbow", "Blood"};
+        String[] descriptions = {"Wizard", "Elf", "Gym Teacher", "Rogue"};
+        for (String name : names) {
+            for (String color : colors) {
+                for (String desc : descriptions) {
+                    System.out.println(name + " the " + color + " " + desc);
+                }
+            }
+        }
+    }
+}
